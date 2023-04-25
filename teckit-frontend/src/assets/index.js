4ec16596075a7b54261f5c4ed5a1a0faff0dbfe3
Kshitij Chaturvedi <98537053+Kshitijk14@users.noreply.github.com>
2023-04-25 12:50:59 +0530

    Add files via upload

---
+++ teckit-frontend/src/assets/index.js
@@ -0,0 +1,53 @@
+import airbnb from "./airbnb.png";
+import binance from "./binance.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import arrowUp from "./arrow-up.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+
+import group1 from "./Group 481776.png";
+import group2 from "./Group 481775.png";
+import logo1 from "./logotech.png";
+import logo2 from "./logo2.svg";
+import img1 from "./img-main.png";
+
+export {
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  arrowUp,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+
+  group1,
+  group2,
+  logo1,
+  logo2,
+  img1,
+};
